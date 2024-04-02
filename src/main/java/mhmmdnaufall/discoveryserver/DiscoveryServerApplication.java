7786de1bd454b3f92cb576cf1b81121e9f4e31e4
Muhammad Naufal <muhammadnaufaall@gmail.com>
2024-04-02 15:01:10 +0700

    Project init

---
+++ src/main/java/mhmmdnaufall/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package mhmmdnaufall.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+}
